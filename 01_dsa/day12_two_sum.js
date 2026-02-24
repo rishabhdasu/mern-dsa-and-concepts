2079ea934543c96e831a87e1e843cb5944fdd383
Rishabh <rishabhdasu@gmail.com>
2026-02-24 23:48:26 +0530

    docs(learning): add problem statement and test case for Two Sum algorithm

--- 01_dsa/day12_two_sum.js
+++ 01_dsa/day12_two_sum.js
@@ -1,3 +1,16 @@
+/*
+Given an array of integers nums and an integer target, return the indices of the two numbers such that they add up to the target.
+
+Constraints:
+You may assume that each input would have exactly one solution.
+You may not use the same element twice.
+You can return the answer in any order.
+
+Example:
+Input: nums = [2, 7, 11, 15], target = 9
+Output: [0, 1] (Because nums[0] + nums[1] == 9)
+*/
+
 const twoSum = (nums, target) => {
   const map = {}; 
 
@@ -11,4 +24,7 @@ const twoSum = (nums, target) => {
     }
     map[currentNum] = i;
   }
-};
+  return null;
+};  
+
+console.log(twoSum([4,21, 8, 34, 6, 12, 51], 20));
